6f6ef3e28d9ef7aff2e57b9f87667a1aa3d52a17
tobiaszirngibl <tobias.zirngibl3011@googlemail.com>
2018-01-29 23:39:05 +0100

    Add action differentiation

--- index.js
+++ index.js
@@ -37,26 +37,31 @@ app.post('/webhook', function (req, res) {
   // the value of Action from api.ai is stored in req.body.result.action
   console.log('* Received action -- %s', req.body.result.action)
 
-    var userName = "";
-  // parameters are stored in req.body.result.parameters
-  var action = req.body.result.parameters['food-substitute'];
+        if(req.body.result.action === "food.substitute") {
 
-    var webhookReply = 'Hello! Here is a substitute list for '+action+': ';
-    unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/ingredients/substitutes?ingredientName="+action)
-        .header("X-Mashape-Key", "eB4slA65XimshJw9xMYuRG4XJ5qdp1vzOF2jsnzAGxOioS6ugP")
-        .header("X-Mashape-Host", "spoonacular-recipe-food-nutrition-v1.p.mashape.com")
-        .end(function (result) {
-            for (var i in result.body.substitutes) {
-                val = result.body.substitutes[i];
-                webhookReply += val + " or ";
-            }
-            res.status(200).json({
-                source: 'webhook',
-                speech: webhookReply,
-                displayText: webhookReply
-            })
 
-        });
+            var userName = "";
+            // parameters are stored in req.body.result.parameters
+            var action = req.body.result.parameters['food-substitute'];
+
+            var webhookReply = 'Hello! Here is a substitute list for ' + action + ': ';
+            unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/ingredients/substitutes?ingredientName=" + action)
+                .header("X-Mashape-Key", "eB4slA65XimshJw9xMYuRG4XJ5qdp1vzOF2jsnzAGxOioS6ugP")
+                .header("X-Mashape-Host", "spoonacular-recipe-food-nutrition-v1.p.mashape.com")
+                .end(function (result) {
+                    for (var i in result.body.substitutes) {
+                        val = result.body.substitutes[i];
+                        webhookReply += val + " or ";
+                    }
+                    res.status(200).json({
+                        source: 'webhook',
+                        speech: webhookReply,
+                        displayText: webhookReply
+                    })
+
+                });
+        }
+
 })
 
 app.listen(app.get('port'), function () {
