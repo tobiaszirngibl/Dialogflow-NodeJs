04014afd6e939a98b1e299f963a21ae3ea46bf36
tobiaszirngibl <tobias.zirngibl3011@googlemail.com>
2018-01-30 22:22:21 +0100

    Added Plan

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ app.get('/webhook', function (req, res) {
 app.post('/webhook', function (req, res) {
   // we expect to receive JSON data from api.ai here.
   // the payload is stored on req.body
-  console.log(req.body)
 
   // we have a simple authentication
   if (REQUIRE_AUTH) {
@@ -37,30 +36,64 @@ app.post('/webhook', function (req, res) {
   // the value of Action from api.ai is stored in req.body.result.action
   console.log('* Received action -- %s', req.body.result.action)
 
-        if(req.body.result.action === "food.substitute") {
+
+      switch(req.body.result.action) {
 
 
-            var userName = "";
-            // parameters are stored in req.body.result.parameters
-            var action = req.body.result.parameters['food-substitute'];
+          case "food.substitute":
+              var action = req.body.result.parameters['food-substitute'];
+              var webhookReply = 'Hello! Here is a substitute list for ' + action + ': ';
 
-            var webhookReply = 'Hello! Here is a substitute list for ' + action + ': ';
-            unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/ingredients/substitutes?ingredientName=" + action)
-                .header("X-Mashape-Key", "eB4slA65XimshJw9xMYuRG4XJ5qdp1vzOF2jsnzAGxOioS6ugP")
-                .header("X-Mashape-Host", "spoonacular-recipe-food-nutrition-v1.p.mashape.com")
-                .end(function (result) {
-                    for (var i in result.body.substitutes) {
-                        val = result.body.substitutes[i];
-                        webhookReply += val + " or ";
-                    }
-                    res.status(200).json({
-                        source: 'webhook',
-                        speech: webhookReply,
-                        displayText: webhookReply
-                    })
+              unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/ingredients/substitutes?ingredientName=" + action)
+                  .header("X-Mashape-Key", "eB4slA65XimshJw9xMYuRG4XJ5qdp1vzOF2jsnzAGxOioS6ugP")
+                  .header("X-Mashape-Host", "spoonacular-recipe-food-nutrition-v1.p.mashape.com")
+                  .end(function (result) {
+                      for (var i in result.body.substitutes) {
+                          val = result.body.substitutes[i];
+                          webhookReply += val + " or ";
+                      }
+                      res.status(200).json({
+                          source: 'webhook',
+                          speech: webhookReply,
+                          displayText: webhookReply
+                      })
 
-                });
-        }
+                  });
+              break;
+
+          case "food.plan":
+
+              var diet = req.body.result.parameters['diet'];
+              var calories = req.body.result.parameters['calories'];
+              var timespan = req.body.result.parameters['timespan'];
+
+              console.log(diet);
+              console.log(calories);
+              console.log(timespan);
+              var webhookReply = 'Hello!: ';
+
+              unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/mealplans/generate?diet="+diet+"&exclude=shellfish%2C+olives&targetCalories="+calories+"&timeFrame="+timespan)
+                  .header("X-Mashape-Key", "KW1you8CYtmshogLM9mgGuL0OyYXp1t4glDjsnNJi6dLuPQUvo")
+                  .header("Accept", "application/json")
+                  .end(function (result) {
+                          webhookReply += result.toString();
+
+                      res.status(200).json({
+                          source: 'webhook',
+                          speech: webhookReply,
+                          displayText: webhookReply
+                      })
+
+                  });
+
+              break;
+
+          default:
+              var i = "";
+              break;
+      }
+
+
 
 })
 
